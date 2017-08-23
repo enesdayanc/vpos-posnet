28c5909119d3c2e18e983f40e8e118a0b49879fc
enesdayanc <enesdayanc@gmail.com>
2017-08-23 10:21:50 +0300

    create RedirectFormMethod const

---
+++ src/Constant/RedirectFormMethod.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enesdayanc
+ * Date: 23/08/2017
+ * Time: 10:21
+ */
+
+namespace PaymentGateway\VPosPosnet\Constant;
+
+
+class RedirectFormMethod
+{
+    const GET = 'GET';
+    const POST = 'POST';
+}
