c9a6c62a479ba66e817b9a15bfdc5f29513cca3b
enesdayanc <enesdayanc@gmail.com>
2017-08-23 10:21:00 +0300

    create RedirectForm model

---
+++ src/Model/RedirectForm.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enesdayanc
+ * Date: 23/08/2017
+ * Time: 10:20
+ */
+
+namespace PaymentGateway\VPosPosnet\Model;
+
+
+class RedirectForm
+{
+    private $action;
+    private $method;
+    private $parameters;
+
+    /**
+     * @return mixed
+     */
+    public function getAction()
+    {
+        return $this->action;
+    }
+
+    /**
+     * @param mixed $action
+     */
+    public function setAction($action)
+    {
+        $this->action = $action;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    /**
+     * @param mixed $method
+     */
+    public function setMethod($method)
+    {
+        $this->method = $method;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
+
+    /**
+     * @param mixed $parameters
+     */
+    public function setParameters($parameters)
+    {
+        $this->parameters = $parameters;
+    }
+}
